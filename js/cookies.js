2a3d3406dc2739f0f1d0d2aaa4f3a73d4936c6f0
Ashaya Sharma <ashayas@github.com>
2016-03-28 02:02:24 -0700

    Added cookies

---
+++ js/cookies.js
@@ -0,0 +1,22 @@
+$(function(){
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+    var user=getCookie("honeyUser");
+    var pass=getCookie("honeyPassword");
+
+    if (user != "") {
+        window.alert("Welcome again " + user + ' ' + pass);
+    } else {
+        window.alert("NOPE");
+    }
+});
